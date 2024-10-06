2d4601e149a49a6a993f3148fc35ccec66d8567e
Dang Le Hai <haidangle678@gmail.com>
2024-10-06 20:17:56 +0700

    Fix bugs

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,5 +4,9 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        field = ["if", "username", "password"]
+        fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
